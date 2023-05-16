7c6c1c8ff0d4cfaf03a40ae448716f14c5c55eab
Atty Cronin <antoniajcronin@gmail.com>
2023-05-16 18:31:53 +0100

    implement getFillings, isFromManchester and getBusNumbers in JS challenge 002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,31 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+
+  if (people <= 40) return 1;
+
+  if (people > 40) {
+    let extraPeople = people % 40;
+    if (extraPeople === 0) return people / 40;
+    if (extraPeople !== 0) return (people - extraPeople) / 40 + 1;
+  }
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
 }

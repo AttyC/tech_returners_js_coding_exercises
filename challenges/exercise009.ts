1bb7f3047a1f0e66500ec77a844f094798b0e612
Atty Cronin <antoniajcronin@gmail.com>
2023-06-05 16:41:07 +0100

    Add type string to generateInitials

--- challenges/exercise009.ts
+++ challenges/exercise009.ts
@@ -6,3 +6,9 @@ export function capitalize(word: Word) {
   if (word === undefined) throw new Error("word is required");
   return word.substring(0, 1).toUpperCase() + word.substring(1);
 }
+
+export function generateInitials(firstName: string, lastName: string): string {
+  if (firstName === undefined) throw new Error("firstName is required");
+  if (lastName === undefined) throw new Error("lastName is required");
+  return `${firstName.slice(0, 1)}.${lastName.slice(0, 1)}`;
+}

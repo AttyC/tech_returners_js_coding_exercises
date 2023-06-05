473fb8fe6ed93248e0bdf048faa9fa9b5300ce70
Atty Cronin <antoniajcronin@gmail.com>
2023-06-05 16:47:11 +0100

    Add type number / string to simpleFizzBuzz

--- challenges/exercise009.ts
+++ challenges/exercise009.ts
@@ -64,3 +64,11 @@ export function getMeanScore(scores: [number]) {
     runningTotal + score;
   return parseFloat((scores.reduce(findTotal) / scores.length).toFixed(2));
 }
+
+export function simpleFizzBuzz(number: number): string {
+  if (number === undefined) throw new Error("number is required");
+  if (number % 3 === 0 && number % 5 === 0) return "fizzbuzz";
+  if (number % 3 === 0) return "fizz";
+  if (number % 5 === 0) return "buzz";
+  return number.toString();
+}
